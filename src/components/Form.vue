c9b25b10d92e8ac234450a6440d07cb5d303d99a
Enzo Polazi <enzopolazi@gmail.com>
2021-09-19 21:26:49 -0300

    Formulário finalizado

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,25 +1,64 @@
 <template>
-    <p>Olá {{ visitor }}</p>
+    <p id="welcoming">Olá {{ visitor }}</p>
     
     <div class="container">
         <div class="row content">
-            <div class="col-md-6">
+            <div class="col">
                 <h3 class="signup-text mb-3">Cadastre-se</h3>
                 <form>
+                    <div class="form-group">
+                        <label for="user">Nome</label>
+                        <input type="user" name="nome" class="form-control">
+                    </div>
                     <div class="form-group">
                         <label for="email">Email</label>
                         <input type="email" name="email" class="form-control">
                     </div>
+                    <h5>Endereço:</h5>
+                    <div class="row content-address">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="pais">País</label>
+                                <input type="text" name="pais" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="municipio">Município</label>
+                                <input type="text" name="municipio" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="estado">Estado</label>
+                                <input type="text" name="estado" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="cep">CEP</label>
+                                <input type="text" name="cep" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-9 form-group">
+                            <label for="rua">Rua</label>
+                            <input type="text" name="rua" class="form-control">
+                        </div>
+                        <div class="col-md-3 form-group">
+                            <label for="numero">Número</label>
+                            <input type="text" name="numero" class="form-control">
+                        </div>
+                        <div class="col form-group">
+                            <label for="complemento">Complemento</label>
+                            <input type="text" name="complemento" class="form-control">
+                        </div>
+                    </div>
                     <div class="form-group">
-                        <label for="user">Usuário</label>
-                        <input type="user" name="usuario" class="form-control">
+                        <label for="cpf">CPF</label>
+                        <input type="text" name="cpf" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="pis">PIS</label>
+                        <input type="text" name="pis" class="form-control">
                     </div>
                     <div class="form-group">
                         <label for="senha">Senha</label>
-                        <input type="password" name="senha" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="senha">Confimar senha</label>
                         <input type="password" name="senha" class="form-control">
                     </div>
                     <button class="btn btn-class">Cadastrar-se</button>
@@ -39,29 +78,36 @@ export default {
 </script>
 
 <style scoped>
+#welcoming{
+    text-align: center;
+}
 .content{
-    margin: 8%;
-    background-color: #261132;
-    padding: 4rem 1rem 4rem 1rem;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 2rem 1rem 2rem 1rem;
     box-shadow: 0px 0px 5px 5px rgba(0,0,0, .05);
     border-radius: 10px;
 }
+.content-address{
+    margin: 10px;
+    border-radius: 5px;
+}
 .signup-text{
-    font-style: normal;
-    
+    font-weight: bold;
+    text-align: center;
 }
 .form-control{
     display: block;
     width: 100%;
     font-size: 1rem;
-    font-weight: 400;
+    font-weight: bold;
     line-height: 1.5;
     border-color: #261132;
     border-style: solid;
-    padding: 1px;
+    padding: 2px;
     color: #495057;
     height: auto;
-    border-radius: 15px;
+    border-radius: 7px;
     background-color: #fff;
     background-clip: padding-box;
     text-indent: 12px;
@@ -73,6 +119,8 @@ export default {
     box-shadow: none;
 }
 .btn-class{
+    display: block;
+    margin: 30px auto auto auto;
     border-color: #261132;
     color: #261132;
 }
